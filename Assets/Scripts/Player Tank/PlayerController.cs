552fca172e250a05de1e2892a8404b05643a9fe1
Dino <84743074+LeapersEdge@users.noreply.github.com>
2023-12-04 09:41:49 +0100

    added basic player controller

--- Assets/Scripts/Player Tank/PlayerController.cs
+++ Assets/Scripts/Player Tank/PlayerController.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -8,40 +10,70 @@ public class PlayerController : MonoBehaviour
 
     [Header("Objects")]
     [SerializeField] GameObject tankHead;
-    [SerializeField] GameObject tankShell;
-    GameObject tankBarell;
+    [SerializeField] GameObject tankShellPrefab;
+    [SerializeField] GameObject tankShellSpawnLocation;
+    [SerializeField] GameObject tankBarrel;
     [Header("Player Movement")]
     [SerializeField] float speed = 125f;
     [SerializeField] float turnSpeed = 125f;
     [SerializeField] float shellSpeed = 125f;
+    [Header("Head Movement")]
+    [SerializeField] float headTurnSpeed = 75f;
+    [SerializeField] float barrelTurnSpeed = 45f;
+    [SerializeField] float barrelMinAngle = -10f;
+    [SerializeField] float barrelMaxAngle = 45f;
+
+    Vector3 velocity;
+    Vector3 acceleration;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        tankBarell = tankHead.transform.GetChild(0).gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
-        // movement
-        rb.velocity = new Vector3(0, 0, Input.GetAxis("Vertical") * speed);
-        if (Input.GetButton("Horizontal"))
-        {
-            this.transform.rotation = Quaternion.Euler(0, Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime, 0);
+    #region MOVEMENT
+        
+        // first we rotate body if needed, than move forward, afterwards rotate head if needed
+
+        if (Input.GetButton("Horizontal") && Input.GetButton("Vertical"))
+        {   
+            transform.Rotate(0, Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime, 0);
         }
+        velocity = transform.forward * Input.GetAxis("Vertical") * speed;
+        
         if (Input.GetButton("Horizontal Hat"))
         {
-            tankHead.transform.Rotate(0, Input.GetAxis("Horizontal Hat") * turnSpeed * Time.deltaTime, 0);
+            tankHead.transform.Rotate(0, Input.GetAxis("Horizontal Hat") * headTurnSpeed * Time.deltaTime, 0);
         }
+        if (Input.GetButton("Vertical Hat"))
+        {
+            tankBarrel.transform.Rotate(-Input.GetAxis("Vertical Hat") * barrelTurnSpeed * Time.deltaTime, 0, 0);
+            if (tankBarrel.transform.rotation.eulerAngles.x + barrelMinAngle > 0 && tankBarrel.transform.rotation.eulerAngles.x < 180)
+            {
+                tankBarrel.transform.rotation = Quaternion.Euler(-barrelMinAngle, tankBarrel.transform.rotation.eulerAngles.y, tankBarrel.transform.rotation.eulerAngles.z);
+            }
+            else if (tankBarrel.transform.rotation.eulerAngles.x < 360 - barrelMaxAngle && tankBarrel.transform.rotation.eulerAngles.x > 180)
+            {
+                tankBarrel.transform.rotation = Quaternion.Euler(360 - barrelMaxAngle, tankBarrel.transform.rotation.eulerAngles.y, tankBarrel.transform.rotation.eulerAngles.z);
+            }
+        }
+    
+    #endregion
 
         if (Input.GetButtonDown("Fire1"))
         {
-            GameObject shellSpawnLocation = tankBarell.transform.GetChild(0).gameObject;
-            GameObject shell = Instantiate(tankShell, shellSpawnLocation.transform.position, shellSpawnLocation.transform.rotation);
+            GameObject shell = Instantiate(tankShellPrefab, tankShellSpawnLocation.transform.position, tankShellSpawnLocation.transform.rotation);
             shell.GetComponent<TankShellController>().shellSpeed = shellSpeed;
-            shellSpawnLocation.GetComponent<AudioSource>().Play();
+            //tankShellSpawnLocation.GetComponent<AudioSource>().Play();
         }
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(transform.position + velocity * Time.fixedDeltaTime);
     }
 }
